cfcc3fbd2e24afaa0f6476200705fec6c89bffde
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-09 23:03:11 +0300

    navbar and index.html contents

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'application',
+    'cloudinary',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+          'USER': 'moringa',
+    'PASSWORD':'12345',
     }
 }
 
