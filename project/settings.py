748717d75762ff1a8c567c51ee04ebfdfca8d053
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-14 12:25:27 +0300

    change mode in .env

--- project/settings.py
+++ project/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-MODE=config("MODE", default="prod")
+MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
-if config('MODE')=="prod":
+if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
